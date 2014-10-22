82876f7a9bc47bc0e4b617a2837d375b109dcb85
Michael Matuzak <mmatuzak@gmail.com>
2014-10-22 14:09:12 -0700

    adding post-install script and workshops package.json

---
+++ post-install.js
@@ -0,0 +1,50 @@
+var spawn = require('child_process').spawn;
+var fstream = require('fstream');
+var fs = require('fs');
+var zlib = require('zlib');
+var tar = require('tar');
+var mkdirp = require('mkdirp');
+var ls = require('ls-stream');
+var through = require('through');
+
+process.chdir('workshops');
+
+var npm = spawn('npm', ['i']);
+
+npm.stdout.on('data', function (data) {
+    console.log(data.toString());
+});
+
+npm.stderr.on('data', function (data) {
+    console.log(data.toString());
+});
+
+function skipper(entry) {
+    return (entry.path.split('/').length > 2) || entry.path === 'node_modules/.bin';
+}
+
+npm.on('close', function () {
+    ls('node_modules').pipe(through(function (entry) {
+        var skip = skipper(entry);
+        var json;
+        var str;
+        var packagePath;
+
+        if (skip) {
+            entry.ignore();
+        } else {
+            packagePath = __dirname + '/workshops/' + entry.path + '/package.json';
+            json = require(packagePath);
+            if (json.dependencies) {
+                json.bundledDependencies = json.dependencies;
+                delete json.dependencies;
+                str = JSON.stringify(json);
+                fs.createWriteStream(packagePath).end(str);
+            }
+            fstream.Reader({ 'path': entry.path, 'type': 'Directory' })
+                .pipe(tar.Pack())
+                .pipe(zlib.Gzip())
+                .pipe(fstream.Writer({ 'path': __dirname + '/workshops/' + entry.path.split('/')[1] + '.tgz' }));
+        }
+    }));
+});
